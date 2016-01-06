16af9e39d3416c08ada71f4231dce3f0f58edadb
tomfun <tomfun1990@gmail.com>
2016-01-06 13:21:00 +0200

    tests

--- test/gulp-test.js
+++ test/gulp-test.js
@@ -1,15 +1,13 @@
 'use strict';
 
-var _          = require('lodash')
-    , path       = require('path');
+var _    = require('lodash')
+  , path = require('path')
+  , fs   = require('fs');
 
 var verboseError = function (res) {
-    console.log(res.text);
+  console.log(res.text);
 };
 
-/**
- * root jsdoc
- */
 describe('base functionality', function () {
   //before(function (done) {
   //  this.timeout(25000);
@@ -17,37 +15,47 @@ describe('base functionality', function () {
   //});
 
   describe('test1', function () {
-    it('load gulp', function (done) {
-      var gulp            = require('gulp')
-        , _               = require('lodash')
-        , bower           = require('main-bower-files')
-        , bowerNormalizer = require('gulp-bower-normalize')
-        , rename          = require('gulp-rename')
-        , install         = require('gulp-install')
-        , watch           = require('gulp-watch')
-        , babel           = require('gulp-babel')
-        , uglify          = require('gulp-uglify')
-        , gutil           = require('gulp-util')
-        , gulpif          = require('gulp-if')
-        , rjs             = require('gulp-requirejs-optimize')
-        , twig_compile    = require('./twig-compile')
-        , shell           = require('gulp-shell')
-        , sourcemaps      = require('gulp-sourcemaps')
-        , sass            = require('gulp-sass')
-        , autoprefixer    = require('gulp-autoprefixer')
-        , cmq             = require('gulp-merge-media-queries')
-        , progeny         = require('gulp-progeny')
-        , minifyCss       = require('gulp-minify-css')
-        , browserSync     = require('browser-sync')
-        , minimatch       = require("minimatch");
+    var result;
+    before(function (done) {
+      fs.readFile(__dirname + '/test1data/output/block/store/listing.twig.js', {}, function (err, data) {
+        //console.log('will@@@@: ', err, data);
+        result = data;
+        //console.log('will: \n', data.toString());
+        //console.log(' \n\n ------- \n\n');
+        done(err);
+      })
+    });
+    it('test transform', function (done) {
+      var twig_compile = require('../twig-compile');
+      var config = {
+        module:         'amd',
+        twig:           'twig',
+        compileOptions: {
+          lookPaths: [__dirname + '/test1data/input/'],
+          viewPrefix: 'views/'
+        }
+      };
+      fs.readFile(__dirname + '/test1data/input/block/store/listing.twig', {}, function (err, data) {
+        var file = {
+          isNull:   function () {
+            return false;
+          },
+          isStream: function () {
+            return false;
+          },
+          relative: 'block/store/listing.twig',
+          contents: data,
+          path:     __dirname + '/test1data/input/block/store/listing.twig'
+        };
 
-
-
-
-      request
-          .get('/')
-          .expect(HTTPStatus.OK)
-          .end(done);
+        twig_compile.transform(config, file, "some", function (n, file) {
+          //var have = file.contents.toString();
+          //console.log('have: \n', have);
+          //console.log(' \n\n ------- \n\n');
+          result.equals(file.contents).should.be.equal(true);
+          done(err);
+        });
+      });
     });
 
   });
